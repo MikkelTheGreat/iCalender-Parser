9d24a56886e1644d2f8a294d4624d951bbd7a4a3
MikkelTheGreat <33924773+MikkelTheGreat@users.noreply.github.com>
2017-11-24 11:34:21 +0100

    Initial commit with the first test written (red bar).

---
+++ iCalender-Parser/test/TDD.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+import java.time.LocalDateTime;
+
+import org.junit.Test;
+
+public class TDD {
+
+	@Test
+	public void testInitialiseringAfEventMedStartOgEnd() {
+		
+		//3. Setup + //2. Execute 
+		LocalDateTime start = LocalDateTime.of(2012, 12, 12, 12, 12, 12);
+		LocalDateTime end = LocalDateTime.of(2012, 12, 12, 13, 13, 13);
+		Event event = new Event(start, end);
+		
+		//1. Assert
+		assertEquals(start, event.getStart());
+		assertEquals(end, event.getEnd());
+		
+		//4. Tear-down
+	}
+
+}
